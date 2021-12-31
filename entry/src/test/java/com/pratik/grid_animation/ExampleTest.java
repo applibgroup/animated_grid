a76f19108b74b1be95f0bcd9fae13fc814842504
Pratik Nagalgave <pratiknagalgave1@gmail.com>
2021-12-31 10:38:10 +0530

    initial commit

--- entry/src/test/java/com/pratik/grid_animation/ExampleTest.java
+++ entry/src/test/java/com/pratik/grid_animation/ExampleTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 
 public class ExampleTest {
     @Test
-    public void testBundleName() {
-        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
-        assertEquals("com.pratik.grid_animation", actualBundleName);
+    public void onStart() {
+        //Empty method
     }
 }
--- grid_animation_library/src/test/java/com/pratik/grid_animation_library/ExampleTest.java
+++ grid_animation_library/src/test/java/com/pratik/grid_animation_library/ExampleTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class ExampleTest {
     @Test
-    public void testBundleName() {
-        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
-        assertEquals("com.pratik.grid_animation_library", actualBundleName);
+    public void onStart() {
+        //Empty method
+    }
 }
